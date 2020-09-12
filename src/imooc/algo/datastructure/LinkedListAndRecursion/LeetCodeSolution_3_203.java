e3555df961f6c2e6fbbf175f3477891c53ec42d2
ephraim <ephraimguo@gmail.com>
2020-09-12 18:08:16 +0800

    recursive in details

--- src/imooc/algo/datastructure/LinkedListAndRecursion/LeetCodeSolution_3_203.java
+++ src/imooc/algo/datastructure/LinkedListAndRecursion/LeetCodeSolution_3_203.java
@@ -1,21 +1,41 @@
+/**
+ * Qn 203
+ * Remove all elements from a linked list of integers that have value val.
+ *
+ * Example:
+ *
+ * Input:  1->2->6->3->4->5->6, val = 6
+ * Output: 1->2->3->4->5
+ */
 package imooc.algo.datastructure.LinkedListAndRecursion;
 
+/**
+ * An recursive method implementation
+ */
 public class LeetCodeSolution_3_203 {
     public ListNode removeElements(ListNode head, int val) {
         if (head == null) {
             return null;
         }
 
-        ListNode res = removeElements(head.next, val);
-        if (head.val == val) {
-            return res;
-        } else {
-            head.next = res;
-            return head;
-        }
+//      ListNode res = removeElements(head.next, val);
+        head.next = removeElements(head.next, val);
+//        if (head.val == val) {
+//            return res;
+//        } else {
+//            head.next = res;
+//            return head;
+//        }
+
+        return head.val == val ? head.next : head;
     }
 
     public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 4, 5, 6, 7, 8, 9};
+        ListNode head = new ListNode(nums);
+        System.out.println(head);
 
+        ListNode res = (new LeetCodeSolution_2_203()).removeElements(head, 4);
+        System.out.println(res);
     }
 }
--- src/imooc/algo/datastructure/LinkedListAndRecursion/readme.md
+++ src/imooc/algo/datastructure/LinkedListAndRecursion/readme.md
@@ -33,4 +33,32 @@ private static int sum(int[] arr, int l) {
 > actually now we can see it like
 >
 > 0 -> [ Here is a shorter linked list start from 1 ] so on and so forth.
- 
+```java
+    public ListNode removeElements(ListNode head, int val) {
+        if (head == null) {
+            return null;
+        }
+
+//      ListNode res = removeElements(head.next, val);
+        head.next = removeElements(head.next, val);
+//        if (head.val == val) {
+//            return res;
+//        } else {
+//            head.next = res;
+//            return head;
+//        }
+
+        return head.val == val ? head.next : head;
+```
+
+### I want to see more details of RECURSION...
+##### For the sum operation of array
+![more-detail-recursion](./img/recursive-indetail-sum-of-array.png)
+> The recursive call is same as any other type of function call, just the function is sam as the current function
+> Each time of the call based on a new set of params
+
+#### For remove element from an LinkedList
+![more-detail-rm-el-linkedlist](./img/recursive-indetail-rm-el-linkedlist.png)
+
+#### Let's recall the system stack again
+![recall-system-stack](./img/recall-system-stack.png)
